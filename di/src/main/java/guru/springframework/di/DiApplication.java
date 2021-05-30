4fa0e04e5e122ac91c8850209004cb86c126a9af
Thiago Tavares <tvttavares@gmail.com>
2021-05-30 19:23:25 -0400

    Spring Context

--- di/src/main/java/guru/springframework/di/DiApplication.java
+++ di/src/main/java/guru/springframework/di/DiApplication.java
@@ -1,13 +1,20 @@
 package guru.springframework.di;
 
+import guru.springframework.di.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiApplication.class, args);
-	}
+		ApplicationContext ctx = SpringApplication.run(DiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);	}
 
 }

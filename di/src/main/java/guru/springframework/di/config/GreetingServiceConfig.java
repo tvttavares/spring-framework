7357601f773fa551c061cb812adc1e1472276a74
Thiago Tavares <tvttavares@gmail.com>
2021-06-06 23:04:47 -0400

    Adding YML files

--- di/src/main/java/guru/springframework/di/config/GreetingServiceConfig.java
+++ di/src/main/java/guru/springframework/di/config/GreetingServiceConfig.java
@@ -9,7 +9,6 @@ import guru.springframework.pets.PetServiceFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:di-config.xml")
 @Configuration
 public class GreetingServiceConfig {
